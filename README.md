fab5395c6f64050b3ddc60e4e96c587e8f1c89f5
Richard Chiu <richard.chiu87@gmail.com>
2023-05-25 17:53:36 -0400

    initialize repo

---
+++ README.md
@@ -0,0 +1,27 @@
+# accuenergy-task
+
+This is a coding task for Accuenergy done by Richard Chiu
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>Accuenergy Task</h1>
+    <h3>Richard Chiu</h3>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+<!-- 
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style> -->
