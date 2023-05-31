8e3de496726b30c6cc05abc14d9cdbe5cb4054bf
Richard Chiu <richard.chiu87@gmail.com>
2023-05-31 10:59:41 -0400

    f leaflet prod b

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,6 +4,8 @@ import { db } from "../../firebase";
 import L from "leaflet";
 import "@geoapify/leaflet-address-search-plugin";
 import "leaflet/dist/leaflet.css";
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon.png";
 import {doc, collection, setDoc, onSnapshot} from "firebase/firestore";
 
 const map = ref(null);
