25de88e245e5281fd8590bc6faacd51b51d893f5
Richard Chiu <richardchiu@Richards-MacBook-Air.local>
2023-06-10 22:39:10 -0400

    title change

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -1,6 +1,6 @@
 <template>
   <header class = "flex flex-col items-center w-full justify-center text-primary text-center py-8 ">
-    <h1 class = "pr-3 text-5xl capitalize" >Accuenergy Task</h1>
+    <h1 class = "pr-3 text-5xl capitalize" >Accu Maps</h1>
     <h3 class = "text-2xl pt-2">Richard Chiu</h3>
   </header>
 </template>
